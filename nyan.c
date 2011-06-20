49b88f9f5c8c4938af915a0e1e862ce42e0e37da
blaise <blaise@macbook-ubuntu.(none)>
2011-06-19 23:10:43 -0700

    Added Johnson's memory error handling and took out some unneeded commented code

--- nyan.c
+++ nyan.c
@@ -46,6 +46,7 @@ static void cleanup(void);
 static void clear_screen(void);
 static void draw_cats(unsigned int frame);
 static void draw_sparkles(void);
+static void errout(char *str);
 static void fillsquare(SDL_Surface* surf, int x, int y, int w, int h, Uint32 col);
 static void handle_args(int argc, char** argv);
 static void handle_input(void);
@@ -89,6 +90,8 @@ add_sparkle(void) {
     sparkle_instance* new;
 
     new = malloc(sizeof(sparkle_instance));
+    if(!new)
+        errout("Could not allocate new sparkle instance memory in add_sparkle.");
 
     new->loc.x = screen->w + 80;
     new->loc.y = (rand() % (screen->h + sparkle_img[0]->h)) - sparkle_img[0]->h;
@@ -115,6 +118,9 @@ add_cat(unsigned int x, unsigned int y) {
     cat_instance* new;
 
     new = malloc(sizeof(cat_instance));
+    if(!new)
+        errout("Could not allocate new cat instance memory in add_cat.");
+
     new->loc.x = x;
     new->loc.y = y;
     new->next = NULL;
@@ -167,13 +173,10 @@ draw_cats(unsigned int frame) {
     while (c) {
         pos.x = c->loc.x;
         pos.y = c->loc.y;
-//        pos.w = screen->w;
-//        pos.h = pos.w/cat_img[frame]->h*cat_img[frame]->w;
 
         if(frame < 2)
             pos.y -= 5;
         SDL_BlitSurface( stretch_cat[frame], NULL, screen, &pos );
-//        SDL_SoftStretch(cat_img[frame],NULL,screen,NULL);
         c = c->next;
     }
 }
@@ -189,6 +192,13 @@ draw_sparkles() {
         SDL_BlitSurface( sparkle_img[s->frame], NULL, screen, &pos );
         s = s->next;
     }
+}
+
+static void
+errout (char *str) {
+    if (str)
+        puts(str);
+    exit(1);
 }
 
 static void
